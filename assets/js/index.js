94037beedd54c683837d0fdb27433b1a1d6af3cd
Karlos Souza <46496268+karlossouza@users.noreply.github.com>
2024-09-30 11:14:17 -0300

    codigo de erro

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 
 const atualizar=()=>{
-    return alert(' [Erro:] Não há valores lançados ou informados aqui!');
+    return alert(' [Erro: 004312]  Nescessário inserir os valores / dados antes de classificar ,atualizar ou qualquer outra função!');
 }
 
 atualizar();
 
 const classificar=()=>{
-    return alert(' [Erro:] Nescessário inserir os valores / dados antes de classificar!')
+    return alert(' [Erro: 005312] Planilha vazia, sem valores para impressão! ');
 }
 
 classificar();
