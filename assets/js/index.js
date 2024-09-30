0b69df60d146e2c4b085393c0d5607502d5284d3
Karlos Souza <46496268+karlossouza@users.noreply.github.com>
2024-09-30 11:05:13 -0300

    aviso de erro

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,12 @@
 
-let nome='carlos';
-console.log(`seu nome: ${nome}`);
+const atualizar=()=>{
+    return alert(' [Erro:] Não há valores lançados ou informados aqui!');
+}
+
+atualizar();
+
+const classificar=()=>{
+    return alert(' [Erro:] Nescessário inserir os valores / dados antes de classificar!')
+}
+
+classificar();
